a05e40d429b41bd9448c88690889bcf5f050f63b
om74202 <ommishra74202@gmail.com>
2024-10-15 17:37:10 +0530

    Adding Vercel related files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   plugins: [react()],
 })
+
